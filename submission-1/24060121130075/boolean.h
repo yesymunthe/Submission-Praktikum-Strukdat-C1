d3d5439604375c81830291bb3070df4bf33ece5e
shidki <100418798+shidki@users.noreply.github.com>
2022-09-18 10:15:59 +0700

    Update boolean.h

--- submission-1/24060121130075/boolean.h
+++ submission-1/24060121130075/boolean.h
@@ -2,9 +2,9 @@
 #define boolean_H
 /* Program   : */
 /* Program   : boolean.h */
-/* Deskripsi : */
+/* Deskripsi : realisasi matriks*/
 /* NIM/Nama  : 24060121130075/Muhamad Ridwan Ash'shidqi */
-/* Tanggal   : 13 september 2022*/
+/* Tanggal   : 18 september 2022*/
 /***********************************/
 
 //type boolean macro bahasa C, false=0, true=1
