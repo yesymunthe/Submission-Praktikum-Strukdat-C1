3987931901cafdac711ed43526fd171cdb22e818
Varrel <100354535+Creissant@users.noreply.github.com>
2022-09-18 22:56:19 +0700

    Add files via upload

---
+++ submission-1/24060121130062/boolean.h
@@ -0,0 +1,16 @@
+#ifndef boolean_H
+#define boolean_H 
+/***********************************/
+/* Program   : boolean.h */
+/* Deskripsi : header file modul boolean */
+/* NIM/Nama  : 24060121130062/Varrel */
+/* Tanggal   : 18-09-2022 */
+/***********************************/
+
+//type boolean macro bahasa C, false=0, true=1 
+#define false 0
+#define true  1
+#define boolean unsigned char 
+
+
+#endif
