bcb1a3157feabc6c4d73b61be068db12ec2cd7be
Maulana Pasya Zefanya <88534377+maulanapasya@users.noreply.github.com>
2022-09-19 09:26:36 +0700

    Tugas 1_Maulana Pasya Zefanya_24060121140175
    Izin mengumpulkan tugas 1 materi matriks. Terima kasih kak.

---
+++ submission-1/24060121140175/boolean.h
@@ -0,0 +1,15 @@
+#ifndef boolean_H
+#define boolean_H
+/* Program   : */
+/* Program   : boolean.h */
+/* Deskripsi : */
+/* NIM/Nama  : Maulana Pasya Zefanya/ 24060121140175*/
+/* Tanggal   : 16 September 2022*/
+/***********************************/
+
+//type boolean macro bahasa C, false=0, true=1
+#define false 0
+#define true  1
+#define boolean unsigned char
+
+#endif
